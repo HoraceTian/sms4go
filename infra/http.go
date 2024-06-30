eaedd8f2b3b02ba483f16627507cc96ad1632587
田浩然 <1315077391@qq.com>
2024-06-30 12:13:57 +0800

    便携 http 访问工具

---
+++ infra/http.go
@@ -0,0 +1,150 @@
+package infra
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+)
+
+// PostJson 发送POST JSON请求
+func PostJson(urlStr string, headers map[string]string, body interface{}) (map[string]interface{}, error) {
+	jsonBody, err := json.Marshal(body)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", urlStr, bytes.NewBuffer(jsonBody))
+	if err != nil {
+		return nil, err
+	}
+
+	for key, value := range headers {
+		req.Header.Set(key, value)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(resp.Body)
+
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var result map[string]interface{}
+	err = json.Unmarshal(respBody, &result)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+// PostForm 发送POST表单请求
+func PostForm(urlStr string, headers map[string]string, body map[string]string) (map[string]interface{}, error) {
+	formData := url.Values{}
+	for key, value := range body {
+		formData.Set(key, value)
+	}
+
+	req, err := http.NewRequest("POST", urlStr, strings.NewReader(formData.Encode()))
+	if err != nil {
+		return nil, err
+	}
+
+	for key, value := range headers {
+		req.Header.Set(key, value)
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(resp.Body)
+
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var result map[string]interface{}
+	err = json.Unmarshal(respBody, &result)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+// PostUrl 发送带URL参数的POST请求
+func PostUrl(baseUrl string, headers map[string]string, params map[string]string) (map[string]interface{}, error) {
+	urlWithParams := baseUrl + "?" + buildQuery(params)
+	req, err := http.NewRequest("POST", urlWithParams, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	for key, value := range headers {
+		req.Header.Set(key, value)
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(resp.Body)
+
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var result map[string]interface{}
+	err = json.Unmarshal(respBody, &result)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+// buildQuery 构建URL查询字符串
+func buildQuery(params map[string]string) string {
+	values := url.Values{}
+	for key, value := range params {
+		values.Set(key, value)
+	}
+	return values.Encode()
+}
+
+// SafeSleep 线程睡眠
+func SafeSleep(retryInterval int) {
+	time.Sleep(time.Duration(retryInterval) * time.Second)
+}
